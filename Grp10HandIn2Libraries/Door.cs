7bdcf2cc3702ddb999b15bc9580df22cef57eedd
amalie <201906104@post.au.dk>
2021-03-11 13:49:04 +0100

    Created a lot of methods

--- Grp10HandIn2Libraries/Door.cs
+++ Grp10HandIn2Libraries/Door.cs
@@ -4,5 +4,16 @@ namespace Grp10HandIn2Libraries
 {
     public class Door
     {
+        public void LockDoor()
+        {
+            //Udskriv på display
+            throw new NotImplementedException();
+        }
+
+        public void UnlockDoor()
+        {
+            //Udskriv på display
+            throw new NotImplementedException();
+        }
     }
 }
--- Grp10HandIn2Libraries/ICharger.cs
+++ Grp10HandIn2Libraries/ICharger.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Grp10HandIn2Libraries
 {
+    public class CurrentEventArgs : EventArgs
+    {
+        // Value in mA (milliAmpere)
+        public double Current { set; get; }
+    }
+
     interface ICharger
     {
+        // Event triggered on new current value
+        event EventHandler<CurrentEventArgs> CurrentValueEvent;
+
+        // Direct access to the current current value
+        double CurrentValue { get; }
+
+        // Require connection status of the phone
+        bool Connected { get; }
+
+        // Start charging
+        void StartCharge();
+        // Stop charging
+        void StopCharge();
     }
 }
