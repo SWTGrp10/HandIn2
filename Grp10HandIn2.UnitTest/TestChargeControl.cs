21c1257bb2b671397a742a9c5e04520fac8d99b8
EmmaSpanner <58326826+EmmaSpanner@users.noreply.github.com>
2021-03-25 14:58:33 +0100

    Added more tests for CargeControlTest

--- Grp10HandIn2.UnitTest/TestChargeControl.cs
+++ Grp10HandIn2.UnitTest/TestChargeControl.cs
@@ -5,7 +5,10 @@ using Grp10HandIn2Libraries;
 using NSubstitute;
 using NSubstitute.ReceivedExtensions;
 using NUnit.Framework;
+using System.Threading;
+
 namespace Grp10HandIn2.UnitTest
+
 
 {
     class TestChargeControl
@@ -17,8 +20,6 @@ namespace Grp10HandIn2.UnitTest
         [SetUp]
         public void Setup()
         {
-
-
         }
 
         [Test]
@@ -51,5 +52,78 @@ namespace Grp10HandIn2.UnitTest
             //Assert
             _display.Received().ConnectPhone();
         }
+
+        [Test]
+        public void ChargeControl_StopCharge_ConnectedIsTrue_stopChargeIsCalled()
+        {
+            //Arrange
+            _charger = Substitute.For<ICharger>();
+            _uut = new ChargeControl(_charger);
+            _charger.Connected = true;
+
+            //Act
+            _uut.StopCharge();
+
+            //Assert
+            _charger.Received().StopCharge();
+        }
+
+        [TestCase(false,false)]
+        [TestCase(true,true)]
+        public void ChargeControl_IsConnected_ReturnBoolIsConnected(bool charger_Connected, bool result)
+        {
+            //Arrange
+            _charger = Substitute.For<ICharger>();
+            _uut = new ChargeControl(_charger);
+
+            //Act
+            _charger.Connected = charger_Connected;
+
+
+            //Assert
+            Assert.That(_uut.IsConnected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void ChargeControl_Charging_CurrentIs4_DisplayRecievesFullyCharged()
+        {
+            //Arrange
+            _charger = Substitute.For<ICharger>();
+            _display = Substitute.For<IDisplay>();
+            _uut = new ChargeControl(_charger);
+            _charger.Connected = true;
+
+            //Act
+            _charger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs {Current = 4.7});
+            _charger.CurrentValueEvent += _uut.Charging;
+
+            //Assert
+            _display.Received().FullyCharged();
+        }
+
+
+        [Test]
+        public void ChargeControl_Charging_CurrentIs300_DisplayRecievesOnGoingCharge()
+        {
+            //Arrange
+            _charger = Substitute.For<ICharger>();
+
+            _charger.Connected = true;
+            _display = Substitute.For<IDisplay>();
+
+            _uut = new ChargeControl(_charger);
+            System.Threading.Thread.Sleep(1100);
+
+            //Act
+            _charger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs());
+            _charger.CurrentValueEvent += _uut.Charging;
+
+            
+
+            //Assert
+            _display.Received().OngoingCharge(302.1);
+        }
+
+
     }
 }
